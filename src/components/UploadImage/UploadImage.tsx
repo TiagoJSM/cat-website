26730012af17dcf55801914f846305930ad98f8a
Tiago Martins <tiago_gobackto80s@hotmail.com>
2021-05-05 21:56:38 +0200

    Add error message to image upload page.

--- src/components/UploadImage/UploadImage.tsx
+++ src/components/UploadImage/UploadImage.tsx
@@ -5,7 +5,9 @@ import { connect } from 'react-redux';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
 import catApi from '../../api/catApi';
 
-type State = { picture: any, uploading: boolean };
+import './UploadImage.css'
+
+type State = { picture: any, uploading: boolean, errorMessage: string };
 type Props = RouteComponentProps<any>;
 
 const mapStateToProps = () => { 
@@ -15,7 +17,7 @@ const mapStateToProps = () => {
 class UploadImage extends React.Component<Props, State> {
   constructor(props: any) {
     super(props);
-     this.state = { picture: undefined, uploading: false };
+     this.state = { picture: undefined, uploading: false, errorMessage: null };
   }
 
   onChange = (picture: any[]) => {
@@ -25,37 +27,41 @@ class UploadImage extends React.Component<Props, State> {
 
       if(picture.length > 0)
       {
-        this.setState({ uploading: true})
+        this.setState({ uploading: true });
         catApi.addImage(picture[0])
           .then(res => {
-            this.props.history.push('/')
+            this.props.history.push('/');
           })
           .catch(err => {
-         
+            // aparently axios doesn't provide the error message, mocking one for the purpose of this
+            this.setState({ uploading: false, errorMessage: 'Error uploading image, please try again'});
           });
       }
   }
 
   render = () => {
-    const { uploading } = this.state;
+    const { uploading, errorMessage } = this.state;
       return (
-        uploading ?
-        <Loader
-            type="Puff"
-            color="#00BFFF"
-            height={100}
-            width={100}
-        /> :
-        <ImageUploader
-              withIcon
-              withPreview
-              singleImage
-              buttonText='Choose images'
-              onChange={this.onChange}
-              imgExtension={['.jpg', '.png']}
-              maxFileSize={5242880}
-          />
-      );
+        <div className="upload-image">
+          <div className="error-message">{errorMessage}</div>
+          {uploading ?
+            <Loader
+                type="Puff"
+                color="#00BFFF"
+                height={100}
+                width={100}
+            /> :
+            <ImageUploader
+                  withIcon
+                  withPreview
+                  singleImage
+                  buttonText='Choose images'
+                  onChange={this.onChange}
+                  imgExtension={['.jpg', '.png']}
+                  maxFileSize={5242880}
+              />
+          }
+        </div>);
   }
 }
 
